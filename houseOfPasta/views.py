721ba2c6d01102f94dbe1b52be85cf0cb3a0cae3
Fabio Madeira <fabio_madeira@outlook.com>
2023-02-22 20:06:18 +0000

    Adding views and urls

--- houseOfPasta/views.py
+++ houseOfPasta/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 class PostList(generic.ListView):
     model = Post
-    querySet = Post.objects.filter(statue=1).order_by('created_on')
+    querySet = Post.objects.filter(status=1).order_by('created_on')
     template_name = 'index.html'
     paginate_by = 3
